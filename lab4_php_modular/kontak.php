5537e200ad424095f0cbfe69740cdf8226ab5912
M. AKMAL AL ABDILAH <92352562+akmalabdilah@users.noreply.github.com>
2023-03-28 14:14:44 +0700

    menambahkan file lab4-php_modular/

---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : M. AKMAL AL ABDILAH</p>
+        <p>Nim : 312110034</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : akmalabdilah6@gmail.com</p>
+        <p>No. Telp : 085693550108</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
